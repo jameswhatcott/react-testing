a71f67ca4ed0478ab216d030113c5293518496bf
jameswhatcott <james@whatcott.com>
2024-10-08 14:36:12 -0500

    started working on the fetch for the pokemon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Nav from "./components/nav.jsx";
-import Body from "./components/body.jsx";
+import Nav from "./components/Nav.jsx";
+import Body from "./components/Body.jsx";
+import Search from "./components/Search.jsx";
 
 function App() {
     return (
         <div>
             <Nav />
+            <Search />
             <Body />
         </div>
     );
