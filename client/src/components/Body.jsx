c33f3c4b78e8a1e9a8e39e7e872d5ccf69deb005
jameswhatcott <james@whatcott.com>
2024-10-08 14:19:11 -0500

    changing the game

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Body = () => {
     return (
         <div style={{ textAlign: 'center', padding: '20px' }}>
-            <h1 style={{ color: 'blue' }}>Hello World</h1>
+            <h1 style={{ color: 'blue' }}>Welcome to the Pokedex!</h1>
         </div>
     )
 };
