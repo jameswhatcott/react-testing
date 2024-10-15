cf7607915b8913cd827faa7eace1e96bf94de0ea
jameswhatcott <james@whatcott.com>
2024-10-15 10:52:16 -0500

    added sound for cries

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 function Body({ data }) {
-  return data ? (
-    <div>
-      <h1>{data.name}</h1>
-      {data.sprites && <img src={data.sprites.front_default} alt={data.name} />}
-    </div>
-  ) : null;
-}
+    const playCry = () => {
+        if (data) {
+          const audio = new Audio(data.cries.latest);
+          audio.play();
+        }
+      };
+
+      return data ? (
+        <div>
+          <h1>{data.name}</h1>
+          {data.sprites && <img src={data.sprites.front_default} alt={data.name} />}
+          <button onClick={playCry}>Play Cry</button>
+        </div>
+      ) : null;
+    }
 
 export default Body;
